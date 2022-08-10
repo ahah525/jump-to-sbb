5b6e5d5e11f4cbc0af7a901d066bd75ab78ed6ab
ahah525 <ahah525@naver.com>
2022-08-10 14:26:19 +0900

    feat: QuestionRepository findBySubject 추가

--- src/main/java/com/ll/exam/sbb/QuestionRepository.java
+++ src/main/java/com/ll/exam/sbb/QuestionRepository.java
@@ -2,7 +2,11 @@ package com.ll.exam.sbb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 // <어떤 Entity의 Repository인지, Key의 type>
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
+    List<Question> findBySubject(String subject);
 }
