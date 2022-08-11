812f47997e0ae8b95e0f22153cb02920d98c75e1
ahah525 <ahah525@naver.com>
2022-08-11 14:33:56 +0900

    test: 기존 테스트 QuestionRepositoryTest 로 이동, AnswerRepositoryTest 추가

--- src/test/java/com/ll/exam/sbb/AnswerRepositoryTest.java
+++ src/test/java/com/ll/exam/sbb/AnswerRepositoryTest.java
@@ -2,7 +2,9 @@ package com.ll.exam.sbb;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
-class SbbApplicationTests {
+class AnswerRepositoryTest {
 
 }
