c826bd06455828b825c2e3ddce5dd27fd3d47148
ahah525 <ahah525@naver.com>
2022-08-11 14:39:00 +0900

    test: AnswerRepositoryTest saveAnswer 테스트

--- src/test/java/com/ll/exam/sbb/AnswerRepositoryTest.java
+++ src/test/java/com/ll/exam/sbb/AnswerRepositoryTest.java
@@ -1,10 +1,24 @@
 package com.ll.exam.sbb;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class AnswerRepositoryTest {
+    @Autowired
+    private QuestionRepository questionRepository;
+    @Autowired
+    private AnswerRepository answerRepository;
 
+    @Test
+    void saveAnswer() {
+        Answer a = new Answer();
+        a.setContent("답변");
+        a.setCreateDate(LocalDateTime.now());
+        // when
+        answerRepository.save(a);
+    }
 }
