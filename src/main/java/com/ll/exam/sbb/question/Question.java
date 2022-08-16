011f289213ff05d1828dc0fad2d22d91a4e8318b
ahah525 <ahah525@naver.com>
2022-08-16 14:31:41 +0900

    feat : 질문 상세조회 페이지에 답변 표시

--- src/main/java/com/ll/exam/sbb/question/Question.java
+++ src/main/java/com/ll/exam/sbb/question/Question.java
@@ -29,6 +29,6 @@ public class Question {
     // DB에 컬럼이 생기지 않고 클래스에만 씀(DB 컬럼에는 정보를 1개만 넣을 수 있기 때문)
     // Question 에서 Answer로 쉽게 접근 가능하도록(굳이 안해도 됨)
     // CascadeType.Remove: Question 삭제하면 Answer 자동 삭제됨
-    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
     private List<Answer> answerList = new ArrayList<>();
 }
