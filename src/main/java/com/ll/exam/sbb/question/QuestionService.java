21697fc8541b80ef34af351e4421494ae31a33e7
ahah525 <ahah525@naver.com>
2022-08-16 11:45:04 +0900

    feat : QuestionController, QuestionService 적용

--- src/main/java/com/ll/exam/sbb/question/QuestionService.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionService.java
@@ -1,14 +1,14 @@
 package com.ll.exam.sbb.question;
 
-import com.ll.exam.sbb.question.Question;
-import com.ll.exam.sbb.question.QuestionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class QuestionService {
-    @Autowired
-    private QuestionRepository questionRepository;
+    private final QuestionRepository questionRepository;
 
     public Question findById(int id) {
         Question q1 = questionRepository.findById(2).get();
@@ -16,5 +16,9 @@ public class QuestionService {
         System.out.println(q2.getAnswerList());
 
         return q2;
+    }
+
+    public List<Question> getList() {
+        return questionRepository.findAll();
     }
 }
