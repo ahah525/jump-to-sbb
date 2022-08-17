c60378e9bb1027d0671738437cd1df3e0d5aa3fd
ahah525 <ahah525@naver.com>
2022-08-17 15:40:52 +0900

    feat : 답변 등록 유효성 검증 추가

--- src/main/java/com/ll/exam/sbb/answer/AnswerController.java
+++ src/main/java/com/ll/exam/sbb/answer/AnswerController.java
@@ -5,10 +5,12 @@ import com.ll.exam.sbb.question.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,9 +20,15 @@ public class AnswerController {
     private final QuestionService questionService;
 
     @PostMapping("/create/{id}")
-    public String save(Model model, @PathVariable("id") int questionId, @RequestParam String content) {
+    public String save(Model model, @PathVariable("id") int questionId, @Valid AnswerForm answerForm, BindingResult bindingResult) {
         Question question = questionService.getQuestion(questionId);
-        answerService.save(question, content);
+
+        // 필수 입력 폼 유효성 검증
+        if ( bindingResult.hasErrors() ) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+        answerService.save(question, answerForm.getContent());
        return String.format("redirect:/question/detail/%s", questionId);
     }
 }
--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -1,5 +1,6 @@
 package com.ll.exam.sbb.question;
 
+import com.ll.exam.sbb.answer.AnswerForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -35,7 +36,7 @@ public class QuestionController {
         return "redirect:/question/list";
     }
 
-    @RequestMapping("/list")
+    @GetMapping("/list")
     // 이 자리에 @ResponseBody가 없으면 resources/question_list/question_list.html 파일을 뷰로 삼는다.
     public String list(Model model) {
         List<Question> questionList = questionService.getList();
@@ -47,8 +48,8 @@ public class QuestionController {
         return "question_list";
     }
 
-    @RequestMapping("/detail/{id}")
-    public String detail(@PathVariable("id") int id, Model model) {
+    @GetMapping("/detail/{id}")
+    public String detail(@PathVariable("id") int id, Model model, AnswerForm answerForm) {
         Question question = questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
@@ -57,7 +58,6 @@ public class QuestionController {
     // 리다이렉트
     @RequestMapping("/")
     public String root() {
-        System.out.println("dd");
         return "redirect:/question/list";
     }
 }
