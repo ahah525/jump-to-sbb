eda2cd28d8f056ebc728ca99ed9ce61992c58742
ahah525 <ahah525@naver.com>
2022-08-17 14:39:02 +0900

    feat : 질문 등록 유효성 검증 추가

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -3,11 +3,13 @@ package com.ll.exam.sbb.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -17,12 +19,18 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("/create")
-    public String createForm() {
+    public String createForm(QuestionForm questionForm) {
+        // 질문 등록 폼의 th:object 때문에 QuestionForm 객체를 매개변수로 받아야 함
         return "question_form";
     }
 
     @PostMapping("/create")
-    public String create(QuestionForm questionForm) {
+    public String create(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        // 유효성 검증에서 에러가 발견되면 폼으로 돌아가기
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        // 질문 저장 후 질문 리스트로 리다이렉트
         questionService.save(questionForm);
         return "redirect:/question/list";
     }
