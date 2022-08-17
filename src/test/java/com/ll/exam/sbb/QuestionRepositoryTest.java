b845500524a575e1a6c7ad0b4338e49c0d7be6e8
ahah525 <ahah525@naver.com>
2022-08-18 00:15:56 +0900

    test : 페이지 함수 테스트

--- src/test/java/com/ll/exam/sbb/QuestionRepositoryTest.java
+++ src/test/java/com/ll/exam/sbb/QuestionRepositoryTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -107,6 +110,15 @@ class QuestionRepositoryTest {
         List<Question> questions = questionRepository.findBySubjectLike("%제목%");
         // then
         assertThat(questions.size()).isEqualTo(2);
+    }
+
+    @Test
+    void findAllPageable() {
+        // Pageble : 한 페이지에 몇개의 아이템이 나와야 하는지 + 현재 몇 페이지인지)
+        Pageable pageable = PageRequest.of(0, lastSampleDataId);
+        Page<Question> page = questionRepository.findAll(pageable);
+
+        assertThat(page.getTotalPages()).isEqualTo(1);
     }
 
     @Test
