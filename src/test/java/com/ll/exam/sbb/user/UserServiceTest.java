9cbbb8cab6f8c8eb90ea54d40391e03fa3b215a1
ahah525 <ahah525@naver.com>
2022-08-18 14:40:01 +0900

    feat : 회원가입 테스트

---
+++ src/test/java/com/ll/exam/sbb/user/UserServiceTest.java
@@ -0,0 +1,20 @@
+package com.ll.exam.sbb.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserServiceTest {
+    @Autowired
+    private UserService userService;
+
+    @Test
+    @DisplayName("회원가입이 가능하다.")
+    void create() {
+        userService.create("user1", "user1@email.com", "1234");
+    }
+}
