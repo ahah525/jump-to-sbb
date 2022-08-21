c274fe5c268037b861cd5926293b8788632d7bed
ahah525 <ahah525@naver.com>
2022-08-21 22:52:56 +0900

    test : 테스트 전 user 삭제 및 Question, Answer 저장 시 작성자 정보 추가

--- src/test/java/com/ll/exam/sbb/UserServiceTest.java
+++ src/test/java/com/ll/exam/sbb/UserServiceTest.java
@@ -1,20 +1,35 @@
 package com.ll.exam.sbb.user;
 
+import com.ll.exam.sbb.answer.AnswerRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class UserServiceTest {
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        clearData();
+        createSampleData();
+    }
+
+    // 외부용
+    public static void clearData() {
+        
+    }
 
     @Test
     @DisplayName("회원가입이 가능하다.")
     void create() {
         userService.create("user1", "user1@email.com", "1234");
     }
+
+
 }
