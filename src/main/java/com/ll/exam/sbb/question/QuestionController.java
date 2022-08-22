8825962490cf91a1eb44b4f10c519a5060089d93
ahah525 <ahah525@naver.com>
2022-08-22 14:45:00 +0900

    feat : 질문 수정 폼 처리

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -5,11 +5,13 @@ import com.ll.exam.sbb.user.SiteUser;
 import com.ll.exam.sbb.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.security.Principal;
@@ -62,6 +64,24 @@ public class QuestionController {
         model.addAttribute("question", question);
         return "question_detail";
     }
+
+    // 게시글 수정폼
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String modifyForm(@PathVariable("id") Long id, QuestionForm questionForm, Principal principal) {
+        // 받아오기
+        Question question = questionService.getQuestion(id);
+        // 작성자 확인하기
+        if (!(question.getAuthor().getUsername()).equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+
+        questionForm.setSubject(question.getSubject());
+        questionForm.setContent(question.getContent());
+
+        return "question_form";
+    }
+
 
     // 리다이렉트
     @RequestMapping("/")
